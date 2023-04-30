42e20b2c073cd9a61b8d065804c44746d254843c
Allen Tan <algtan.dev@gmail.com>
2023-04-30 12:21:52 -0700

    Remove setters from GraphDBNode

--- proj3/src/main/java/GraphDB.java
+++ proj3/src/main/java/GraphDB.java
@@ -31,24 +31,16 @@ public class GraphDB {
         private double lon;
 
         GraphDBNode(double lat, double lon) {
-            setLat(lat);
-            setLon(lon);
+            this.lat = lat;
+            this.lon = lon;
         }
 
         public double getLat() {
             return lat;
         }
 
-        public void setLat(double lat) {
-            this.lat = lat;
-        }
-
         public double getLon() {
             return lon;
-        }
-
-        public void setLon(double lon) {
-            this.lon = lon;
         }
     }
 
